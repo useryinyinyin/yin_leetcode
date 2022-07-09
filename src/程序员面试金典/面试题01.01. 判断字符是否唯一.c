5d1f90975f70fe3373a3636213035ad9f9ac1cbb
useryinyinyin <2621715498@qq.com>
2022-07-10 00:32:03 +0800

    feat:面试题01.01 判断字符是否唯一 DIR:Issue1

---
+++ src/程序员面试金典/面试题01.01. 判断字符是否唯一.c
@@ -0,0 +1,22 @@
+int cmp(const void *_a, const void *_b)
+{
+    char *a = (char *)_a;
+    char *b = (char *)_b;
+    return *a - *b;
+}
+
+bool isUnique(char* astr){
+
+    if (strlen(astr) == 0) {
+        return true;
+    }
+    qsort(astr, strlen(astr) - 1, sizeof(char), cmp);
+
+    for (int i = 0; i < strlen(astr); i++) {
+        if (astr[i] == astr[i + 1]) {
+            return false;
+        }
+    }
+
+    return true;
+}
