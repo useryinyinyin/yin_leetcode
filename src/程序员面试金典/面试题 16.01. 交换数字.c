af0ff38ed13104d859b4f2e55c7153c0a3f800d4
useryinyinyin <2621715498@qq.com>
2022-07-12 00:01:26 +0800

    feat:面试题 16.01. 交换数字 DIR:Issue1

---
+++ src/程序员面试金典/面试题 16.01. 交换数字.c
@@ -0,0 +1,12 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* swapNumbers(int* numbers, int numbersSize, int* returnSize){
+
+    numbers[0] ^= numbers[1];
+	numbers[1] ^= numbers[0];
+	numbers[0] ^= numbers[1];
+
+    *returnSize = 2;
+	return numbers;
+}
